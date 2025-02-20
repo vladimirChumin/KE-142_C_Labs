3bc42be1c5a5a4e9812eb0616120d2f7e23f8460
VladlenTar <114384254+VladlenTar@users.noreply.github.com>
2025-02-20 11:46:10 +0500

    initialize project structure with basic C++ calculator and prime number checker

---
+++ cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/Lab1/lab_1.cpp.obj"
+  "CMakeFiles/untitled.dir/Lab1/lab_1.cpp.obj.d"
+  "CMakeFiles/untitled.dir/Lab2/lab_2.cpp.obj"
+  "CMakeFiles/untitled.dir/Lab2/lab_2.cpp.obj.d"
+  "CMakeFiles/untitled.dir/main.cpp.obj"
+  "CMakeFiles/untitled.dir/main.cpp.obj.d"
+  "libuntitled.dll.a"
+  "untitled.exe"
+  "untitled.exe.manifest"
+  "untitled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
