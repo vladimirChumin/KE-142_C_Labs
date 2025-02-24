768509f7461659d2de5e77e57416253c2e39e887
vol <ke2442cvv43@susu.ru>
2025-02-24 21:10:13 +0500

    initialize project structure with basic C++ calculator and prime number checker

--- Lab6/lab_6.cpp
+++ Lab6/lab_6.cpp
@@ -1,103 +1,114 @@
-// Задание 6.1
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+class Polynomial {
+protected:
+    int* coefficients = nullptr;
+    int index;
+    bool isInteractive = false;
 
-//#include <iostream>
-//#include <cmath>  // Для pow()
-//using namespace std;
-//
-//class Polynomial {
-//private:
-//    int degree;        // Степень полинома
-//    double* coeffs;    // Динамический массив коэффициентов
-//
-//public:
-//    // Конструктор
-//    Polynomial(int deg) : degree(deg) {
-//        coeffs = new double[degree + 1]();  // Выделение памяти и инициализация 0
-//    }
-//
-//    // Деструктор
-//    ~Polynomial() {
-//        delete[] coeffs;  // Освобождение памяти
-//    }
-//
-//    // Ввод коэффициентов полинома
-//    void inputCoefficients() {
-//        cout << "Введите коэффициенты от старшего (x^" << degree << ") до младшего (x^0):\n";
-//        for (int i = 0; i <= degree; i++) {
-//            cout << "Коэффициент при x^" << (degree - i) << ": ";
-//            cin >> coeffs[i];
-//        }
-//    }
-//
-//    // Вывод полинома
-//    void display() const {
-//        for (int i = 0; i <= degree; i++) {
-//            if (coeffs[i] != 0) {
-//                cout << coeffs[i] << "x^" << (degree - i);
-//                if (i < degree) cout << " + ";
-//            }
-//        }
-//        cout << endl;
-//    }
-//
-//    // Перегрузка оператора + для сложения двух полиномов
-//    Polynomial operator+(const Polynomial& other) const {
-//        int maxDegree = max(degree, other.degree);  // Находим максимальную степень
-//        Polynomial result(maxDegree);  // Создаем новый полином
-//
-//        // Копируем коэффициенты из текущего полинома
-//        for (int i = 0; i <= degree; i++) {
-//            result.coeffs[maxDegree - degree + i] = coeffs[i];
-//        }
-//
-//        // Складываем коэффициенты из второго полинома
-//        for (int i = 0; i <= other.degree; i++) {
-//            result.coeffs[maxDegree - other.degree + i] += other.coeffs[i];
-//        }
-//
-//        return result;
-//    }
-//
-//    // Вычисление значения полинома при заданном x
-//    double evaluate(double x) const {
-//        double result = 0;
-//        for (int i = 0; i <= degree; i++) {
-//            result += coeffs[i] * pow(x, degree - i);  // Считаем x^степень
-//        }
-//        return result;
-//    }
-//};
-//
-//// Главная функция
-//int main() {
-//    int degree1, degree2;
-//
-//    cout << "Введите степень первого полинома: ";
-//    cin >> degree1;
-//    Polynomial poly1(degree1);
-//    poly1.inputCoefficients();
-//
-//    cout << "Введите степень второго полинома: ";
-//    cin >> degree2;
-//    Polynomial poly2(degree2);
-//    poly2.inputCoefficients();
-//
-//    cout << "\nПервый полином: ";
-//    poly1.display();
-//    cout << "Второй полином: ";
-//    poly2.display();
-//
-//    // Сложение полиномов
-//    Polynomial sum = poly1 + poly2;
-//    cout << "Сумма полиномов: ";
-//    sum.display();
-//
-//    // Вычисление значения полинома
-//    double x;
-//    cout << "\nВведите x для вычисления P(x): ";
-//    cin >> x;
-//    cout << "P(x) = " << poly1.evaluate(x) << endl;
-//
-//    return 0;
-//}
+public:
+    Polynomial(bool isInterective = false) : isInteractive(isInterective) {
+        if (!isInteractive) {
+            this->index = 1;
+            this->coefficients = new int[1]{0};
+        } else {
+            int max_index;
+            cout << "How many coefficients do you want to enter? ";
+            cin >> max_index;
+
+            if (max_index <= 0) max_index = 1;
+
+            int* newCoefficients = new int[max_index];
+
+            for (int i = 0; i < max_index; i++) {
+                cout << "Enter coefficient #" << i + 1 << ": ";
+                cin >> newCoefficients[i];
+            }
+
+            this->index = max_index;
+            this->coefficients = newCoefficients;
+        }
+    }
+
+    Polynomial(int* new_coefficients, int index) : index(index) {
+        this->coefficients = new int[index];
+        for (int i = 0; i < index; i++) {
+            this->coefficients[i] = new_coefficients[i];
+        }
+    }
+
+    Polynomial(int index) {
+        this->index = max(1, index);
+        this->coefficients = new int[this->index]{0};
+    }
+
+    void printPolinomial() {
+        std::string result = "";
+
+        for (int i = 0; i < index; i++) {
+            int coeff = coefficients[i];
+            int power = index - i - 1;
+
+            if (coeff == 0) continue;
+
+            if (!result.empty()) {
+                result += (coeff > 0 ? " + " : " - ");
+            } else if (coeff < 0) {
+                result += "-";
+            }
+
+            if (abs(coeff) != 1 || power == 0) {
+                result += std::to_string(abs(coeff));
+            }
+
+            if (power > 0) {
+                result += "x";
+                if (power > 1) {
+                    result += "^" + std::to_string(power);
+                }
+            }
+        }
+
+        cout << (result.empty() ? "0" : result) << endl;
+    }
+
+    double getPolinomialSum() {
+        int x;
+        double sum = 0;
+
+        cout << "Enter x: ";
+        cin >> x;
+
+        for (int i = 0; i < index; i++) {
+            sum += coefficients[i] * pow(x, (index - i - 1));
+        }
+        return sum;
+    }
+
+    Polynomial operator+(const Polynomial& other) const {
+        int maxIndex = max(1, max(this->index, other.index));
+        Polynomial Result(maxIndex);
+
+        for (int i = 0; i < this->index; i++) {
+            Result.coefficients[maxIndex - this->index + i] += this->coefficients[i];
+        }
+        for (int i = 0; i < other.index; i++) {
+            Result.coefficients[maxIndex - other.index + i] += other.coefficients[i];
+        }
+
+        return Result;
+    }
+
+    ~Polynomial() {
+        delete[] coefficients;
+    }
+};
+
+int main() {
+    Polynomial firstPolynomial(true), secondPolynomial(true);
+    Polynomial sum = firstPolynomial + secondPolynomial;
+    sum.printPolinomial();
+    return 0;
+}
